d6666967010e7d5d108b656d57b18cd60c2a7de9
Rosario <roserranoar@gmail.com>
2022-08-10 21:13:51 -0300

    ejercicio 2

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,20 +1,13 @@
-let nombre = "Rosario Serrano";
 
-let edad = 37;
-console.log(nombre,edad);
-let pregunta= true;
-console.log("¿eres desarrollador?:" + pregunta)
-
-const birthday= new Date(1984,10,5);
-
-console.log(birthday);
-
-const book= {
-    titulo:"El maravilloso Mago de Oz",
-    autor:"L. Frank Baum",
-    anyo:1956,
-    url: "https://es.wikipedia.org/wiki/El_maravilloso_mago_de_Oz#/media/Archivo:Wizard_title_page.jpg",
-    }
-    
-
-console.log(book.titulo,book.autor,book.anyo,book.url)
+const lista = [
+    "Rosario",
+    37,
+    true,
+    new Date(1984, 10, 5),
+    { 
+        titulo: "El maravilloso Mago de Oz",
+        autor: "L. Frank Baum",
+        fecha: new Date(1956, 0, 1),
+        url: "https://es.wikipedia.org/wiki/El_maravilloso_mago_de_Oz#/media/Archivo:Wizard_title_page.jpg"
+      },
+  ];
